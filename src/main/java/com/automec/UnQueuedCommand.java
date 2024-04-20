74c30304769a96a734dabcd465f6b62810659a7c
podrybaum <podrybaum@gmail.com>
2024-04-19 22:06:40 -0500

    UnqueuedCommand warnings fixed and cleaned up dead code.

--- src/main/java/com/automec/UnQueuedCommand.java
+++ src/main/java/com/automec/UnQueuedCommand.java
@@ -10,106 +10,110 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 
 public class UnQueuedCommand {
-   I2CBuffer buffer;
-   I2CBuffer response;
-   public int priority;
+	I2CBuffer buffer;
+	I2CBuffer response;
+	public int priority;
 
-   public UnQueuedCommand(I2CBuffer buffer, int priority) {
-      this.buffer = buffer;
-      this.priority = priority;
-   }
+	public UnQueuedCommand(I2CBuffer buffer, int priority) {
+		this.buffer = buffer;
+		this.priority = priority;
+	}
 
-   public void runCommand() {
-      try {
-         QueuedCommand.lock = true;
-         if (this.buffer.get(1) != 255) {
-            int i;
-            if (this.buffer.get(1) != 83) {
-               String t = "Command: " + String.format("%02X", this.buffer.get(1)) + " Sending: ";
+	public void runCommand() {
+		try {
+			QueuedCommand.lock = true;
+			if (this.buffer.get(1) != 255) {
+				int i;
+				if (this.buffer.get(1) != 83) {
+					String t = "Command: " + String.format("%02X", this.buffer.get(1)) + " Sending: ";
 
-               for(i = 0; i < this.buffer.length; ++i) {
-                  t = t + String.format("%02x", this.buffer.get(i)) + " ";
-               }
+					for (i = 0; i < this.buffer.length; ++i) {
+						t = t + String.format("%02x", this.buffer.get(i)) + " ";
+					}
 
-               Settings.log.finer(t);
-            }
+					Settings.log.finer(t);
+				}
 
-            Communications.bus.write(this.buffer);
-            Thread.sleep(1L);
-            if (this.buffer.get(1) == 83) {
-               this.response = new I2CBuffer(46);
-               Thread.sleep(20L);
-               Communications.bus.read(this.response);
-               if (this.response.get(1) != 21 && this.response.get(0) == 173) {
-                  Communications.statusResponse = this.response;
-                  Communications.updateStatus();
-               } else {
-                  Settings.log.finer("MAC sends NAK");
-               }
-            } else {
-               this.response = new I2CBuffer(2);
-               Communications.bus.read(this.response);
-               if (SystemCommands.isRPi()) {
-                  Thread.sleep(15L);
-               }
+				Communications.bus.write(this.buffer);
+				Thread.sleep(1L);
+				if (this.buffer.get(1) == 83) {
+					this.response = new I2CBuffer(46);
+					Thread.sleep(20L);
+					Communications.bus.read(this.response);
+					if (this.response.get(1) != 21 && this.response.get(0) == 173) {
+						Communications.statusResponse = this.response;
+						Communications.updateStatus();
+					} else {
+						Settings.log.finer("MAC sends NAK");
+					}
+				} else {
+					this.response = new I2CBuffer(2);
+					Communications.bus.read(this.response);
+					if (SystemCommands.isRPi()) {
+						Thread.sleep(15L);
+					}
 
-               if (this.buffer.get(1) == 103) {
-                  ArrayList<Integer> tmp = new ArrayList();
+					if (this.buffer.get(1) == 103) {
+						ArrayList<Integer> tmp = new ArrayList<Integer>();
 
-                  for(i = 0; i < this.response.length; ++i) {
-                     tmp.add(this.response.get(i));
-                  }
-               }
+						for (i = 0; i < this.response.length; ++i) {
+							tmp.add(this.response.get(i));
+						}
+					}
 
-               if (this.response.get(0) == SystemCommands.getTwosComp(this.buffer.get(1)) && this.response.get(1) == 6) {
-                  Communications.bus.write((new I2CBuffer(1)).set(0, 6));
-                  if (this.buffer.get(1) == 83 || this.buffer.get(1) == 88) {
-                     System.out.println("resp: " + this.response.get(0) + ":" + this.response.get(1));
-                  }
+					if (this.response.get(0) == SystemCommands.getTwosComp(this.buffer.get(1))
+							&& this.response.get(1) == 6) {
+						Communications.bus.write((new I2CBuffer(1)).set(0, 6));
+						if (this.buffer.get(1) == 83 || this.buffer.get(1) == 88) {
+							System.out.println("resp: " + this.response.get(0) + ":" + this.response.get(1));
+						}
 
-                  this.response.clear();
-                  if (this.buffer.get(1) == 54) {
-                     if (this.buffer.get(4) == 7) {
-                        RunJobPage.xAxisInitialPosition = ((Axis)Settings.axes.get(0)).getPosition();
-                        if (this.buffer.get(2) == 2) {
-                           RunJobPage.xAxisTargetDirection = AxisDirection.IN;
-                        } else {
-                           RunJobPage.xAxisTargetDirection = AxisDirection.OUT;
-                        }
+						this.response.clear();
+						if (this.buffer.get(1) == 54) {
+							if (this.buffer.get(4) == 7) {
+								RunJobPage.xAxisInitialPosition = ((Axis) Settings.axes.get(0)).getPosition();
+								if (this.buffer.get(2) == 2) {
+									RunJobPage.xAxisTargetDirection = AxisDirection.IN;
+								} else {
+									RunJobPage.xAxisTargetDirection = AxisDirection.OUT;
+								}
 
-                        RunJobPage.xAxisTargetPosition = ByteBuffer.wrap(new byte[]{(byte)this.buffer.get(9), (byte)this.buffer.get(10), (byte)this.buffer.get(11), (byte)this.buffer.get(12)}).getInt();
-                        RunJobPage.xAxisMovingStart = System.currentTimeMillis();
-                        Listener.start.add(RunJobPage.xAxisMovingStart);
-                        RunJobPage.xAxisMoving = true;
-                     } else {
-                        this.buffer.get(4);
-                     }
-                  } else {
-                     this.buffer.get(1);
-                  }
+								RunJobPage.xAxisTargetPosition = ByteBuffer
+										.wrap(new byte[] { (byte) this.buffer.get(9), (byte) this.buffer.get(10),
+												(byte) this.buffer.get(11), (byte) this.buffer.get(12) })
+										.getInt();
+								RunJobPage.xAxisMovingStart = System.currentTimeMillis();
+								Listener.start.add(RunJobPage.xAxisMovingStart);
+								RunJobPage.xAxisMoving = true;
+							}
+						}
 
-                  Settings.log.finer("Command: " + String.format("%02X", this.buffer.get(1)) + " Final response from MAC: " + String.format("%02x", this.response.get(0)) + ":" + String.format("%02x", this.response.get(1)));
-               } else {
-                  Settings.log.finer("MAC SENDS NAK: Command: " + String.format("%02X", this.buffer.get(1)) + " " + String.format("%02x", this.response.get(0)) + ":" + String.format("%02x", this.response.get(1)));
-               }
-            }
-         }
-      } catch (IOException var8) {
-         String t = "IO exception when writing to I2CBuffer: " + var8.getMessage() + " \ncommand: ";
+						Settings.log.finer("Command: " + String.format("%02X", this.buffer.get(1))
+								+ " Final response from MAC: " + String.format("%02x", this.response.get(0)) + ":"
+								+ String.format("%02x", this.response.get(1)));
+					} else {
+						Settings.log.finer("MAC SENDS NAK: Command: " + String.format("%02X", this.buffer.get(1)) + " "
+								+ String.format("%02x", this.response.get(0)) + ":"
+								+ String.format("%02x", this.response.get(1)));
+					}
+				}
+			}
+		} catch (IOException var8) {
+			String t = "IO exception when writing to I2CBuffer: " + var8.getMessage() + " \ncommand: ";
 
-         for(int i = 0; i < this.buffer.length; ++i) {
-            t = t + String.format("%02x", this.buffer.get(i)) + " ";
-         }
+			for (int i = 0; i < this.buffer.length; ++i) {
+				t = t + String.format("%02x", this.buffer.get(i)) + " ";
+			}
 
-         Settings.log.log(Level.WARNING, t, var8);
-      } catch (Exception var9) {
-         Settings.log.log(Level.SEVERE, "Listener", var9);
-      } catch (Throwable e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} finally {
-         QueuedCommand.lock = false;
-      }
+			Settings.log.log(Level.WARNING, t, var8);
+		} catch (Exception var9) {
+			Settings.log.log(Level.SEVERE, "Listener", var9);
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			QueuedCommand.lock = false;
+		}
 
-   }
+	}
 }
